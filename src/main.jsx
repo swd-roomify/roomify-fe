0fcefcc3f4afa9a6c9986b2c070d49ee138f6d29
Hyudeque <quanghuy01062004@gmail.com>
2025-01-10 08:59:05 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import RoutePath from './routes/routePath.jsx'
+import RoutePath from './routes/RoutePath.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <RoutePath/>
+        <React.StrictMode>
+                <RoutePath />
+        </React.StrictMode>
 )
