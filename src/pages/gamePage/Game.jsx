9d848444e06c9584233f70ac845fd2ac9b4fe26b
Hyudeque <quanghuy01062004@gmail.com>
2025-01-18 13:33:42 +0700

    resize the game

--- src/pages/gamePage/Game.jsx
+++ src/pages/gamePage/Game.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Map from '../../components/game/Map';
 import Camera from '../../components/camera/Camera';
+import "../../assets/style/css/game.css"
 
 const Game = () => {
   const [nearbyPlayers, setNearbyPlayers] = useState([]); 
