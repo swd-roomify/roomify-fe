5800b5d1acd33bfd75a3a4b2400ae5321ab39090
Hyudeque <quanghuy01062004@gmail.com>
2025-01-18 13:05:28 +0700

    Remove redundant

--- src/routes/RoutePath.jsx
+++ src/routes/RoutePath.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MenuPage from '../components/menu/MenuPage'
-import App from '../App'
 import Game from '../pages/gamePage/Game'
 
 const RoutePath = () => {
@@ -9,7 +8,6 @@ const RoutePath = () => {
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<MenuPage/>}/>
-            <Route path='/game' element={<App/>}/>
             <Route path='/demo' element={<Game/>}/>
         </Routes>
     </BrowserRouter>
