0274e2fb084186375ecd6c3570e2b64348930228
Hyudeque <quanghuy01062004@gmail.com>
2025-01-19 11:51:20 +0700

    Adding integration

---
+++ src/mediasoupErrorMessage.js
@@ -0,0 +1,14 @@
+const mediasoupErrorMessage = {
+    // system error
+    unknow: "An unknown error occurred, please try to reload the page",
+    unknowServerMessage: "Unhandled socket event",
+
+    // device error
+    loadDeviceFailed: "Failed to load device",
+
+    // transport
+    creatingProducerTransport: "Error creating producer transport",
+
+};
+  
+export default mediasoupErrorMessage;
---
+++ src/mediasoupServerConstants.js
@@ -0,0 +1,40 @@
+const mediasoupServerConstants = {
+
+    sfuServerUrl: import.meta.env.VITE_SFU_SERVER_URL || "http://192.168.0.0:3000",
+    routerRtpCapabilities: "router-rtp-capabilities",
+    connectionstatechange: "connectionstatechange",
+    serverLog: "server-log",
+    err: "error",
+
+    
+    // producer
+    createProducerTransport: "create-producer-transport",
+    producerTransportCreated: "producer-transport-created",
+    closeProducerTransport: "close-producer-transport",
+    producerId: "producer-id",
+
+
+    // consumer
+    createConsumerTransport: "create-consumer-transport",
+    connectConsumerTransport: "connect-consumer-transport",
+    
+
+    // transport
+    transportConnect: "transport-connect",
+    transportProduce: "transport-produce",
+    subTransportCreated: "sub-transport-created",
+
+
+    // utilities
+    subscribed: "subscribed",
+    consume: "consume",
+    connect: "connect",
+    produce: "produce",
+    connecting: "connecting",
+    connected: "connected",
+    failed: "failed",
+    
+};
+  
+export default mediasoupServerConstants;
+  
