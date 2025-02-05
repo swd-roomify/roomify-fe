c42ac2dfdeac1a402b7a8768008a57864885eea1
Hyudeque <quanghuy01062004@gmail.com>
2025-02-05 11:22:48 +0700

    Update RoutePath.jsx

--- src/routes/RoutePath.jsx
+++ src/routes/RoutePath.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MenuPage from '../pages/menu/MenuPage'
 import Game from '../pages/gamePage/Game'
+import JoinPage from '../pages/joinPage/JoinPage'
 
 const RoutePath = () => {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<MenuPage/>}/>
+            <Route path='/game' element={<MenuPage/>}/>
+            <Route path='/join' element={<JoinPage/>}/>
             <Route path='/demo' element={<Game/>}/>
         </Routes>
     </BrowserRouter>
